efd2cac8ab98c4b66cb16e23e97a3135e4c6ea9e
Simon Denier <simon@sogilis.com>
2017-05-01 12:37:51 +0200

    Add Players resource

--- src/Update.elm
+++ src/Update.elm
@@ -2,10 +2,15 @@ module Update exposing (..)
 
 import Messages exposing (Msg(..))
 import Models exposing (Model)
+import Players.Update
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        PlayersMsg subMsg ->
+            let
+                ( updatedPLayers, cmd ) =
+                    Players.Update.update subMsg model.players
+            in
+                ( { model | players = updatedPLayers }, Cmd.map PlayersMsg cmd )
